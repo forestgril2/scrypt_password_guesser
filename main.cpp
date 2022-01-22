722aec17cd140eb78e2525c7edf96308ff3a8586
forestgril <forestgril@yahoo.com>
2022-01-23 00:35:52 +0100

    Verify an Ethereum keystore file password using python script.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
+#include <cstdio>
 #include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
 
-using namespace std;
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    const std::string command = "python3 /home/space/projects/decrypt-ethereum-keyfile/main.py /home/space/projects/scrypt_password_guesser/UTC--2022-01-22T23-28-24.373Z--013efef911d47e09b85f9df956e6565d0f828622 ";
+    const std::string pass = "dupa88ąść";
+
+    const auto result = exec((command + pass).c_str());
+
+    if (result.starts_with("Password verified.\n"))
+        std::cout << "PASS" << std::endl;
+    else
+        std::cout << result << std::endl;
+
     return 0;
 }
