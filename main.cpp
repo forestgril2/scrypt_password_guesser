9ef786dc383b268c320249ade17b1155bb4511b3
forestgril <forestgril@yahoo.com>
2022-01-29 10:04:52 +0100

    Fix for Windows builds.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,13 @@
 std::string exec(const char* cmd) {
     std::array<char, 128> buffer;
     std::string result;
+
+#ifdef _WIN32
+    std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd, "r"), _pclose);
+#else
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+#endif
+
     if (!pipe) {
         throw std::runtime_error("popen() failed!");
     }
@@ -20,7 +26,12 @@ std::string exec(const char* cmd) {
 
 int main()
 {
+#ifdef _WIN32
+    const std::string command = "python D:/Projects/decrypt-ethereum-keyfile/main.py D:/Projects/scrypt_password_guesser/UTC--2022-01-22T23-28-24.373Z--013efef911d47e09b85f9df956e6565d0f828622 ";
+#else
     const std::string command = "python3 /home/space/projects/decrypt-ethereum-keyfile/main.py /home/space/projects/scrypt_password_guesser/UTC--2022-01-22T23-28-24.373Z--013efef911d47e09b85f9df956e6565d0f828622 ";
+#endif
+
     const std::string pass = "dupa88ąść";
 
     const auto result = exec((command + pass).c_str());
